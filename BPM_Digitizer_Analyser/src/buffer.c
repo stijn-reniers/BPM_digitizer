94108094fada049e4011e7efa388b3ecd2deb029
DecosterBram <decoster.bram@hotmail.com>
2021-02-13 16:34:17 +0100

    now with all files

---
+++ BPM_Digitizer_Analyser/src/buffer.c
@@ -0,0 +1,89 @@
+/*
+ * buffer.c
+ *
+ * Created: 10/02/2021 11:12:47
+ *  Author: Decoster
+ */ 
+#include "buffer.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+uint16_t buffer0[buffersize]={ 0 };
+uint16_t buffer1[buffersize]={ 0 };
+uint16_t bufferIndex=0;
+uint16_t buffersFilled=0;
+bool currentbuffer=false;
+bool printed=false;
+
+
+void printFullBuffer(void){
+	if (!printed)
+	{
+		printed=true;
+		for(int i=0; i<buffersize;i++){
+			printf("%u\n\r", buffer0[i]);
+		}
+	}	
+}
+
+void addSample(uint16_t sample){
+	if (bufferIndex<buffersize)
+	{
+		if (currentbuffer)
+		{
+			//puts("filling buffer 1\n");
+			buffer1[bufferIndex]= sample;
+		}else{
+			//puts("filling buffer 0\n");
+			buffer0[bufferIndex]= sample;
+		}
+		bufferIndex++;
+	}
+}
+
+volatile void switchBuffer(void){
+	buffersFilled++;
+	bufferIndex=0;
+	//puts("buffer index reset\n");
+	currentbuffer= !currentbuffer;
+}
+
+void cycleEnded(void){
+	//printf("%u\n\r", bufferIndex);
+	if (bufferIndex<buffersize)
+	{
+		for (int i=bufferIndex;i<buffersize;i++)
+		{
+			addSample(0);
+		}
+	} 
+	
+	switchBuffer();
+	
+}
+
+uint16_t getbuffersFilled(){
+	return buffersFilled;
+}
+
+void testPrint(void){
+	puts("first buffer:\n\r");
+	for(int i=0; i<buffersize;i++){
+		printf("%u\n\r", buffer0[i]);
+	}
+	puts("second buffer:\n\r");
+	for(int i=0; i<buffersize;i++){
+		printf("%u\n\r", buffer1[i]);
+	}
+}
+
+uint16_t* getFilledBuffer(void){
+	if (currentbuffer)
+	{
+		return buffer0;
+	} 
+	else
+	{
+		return buffer1;
+	}
+}
---
+++ BPM_Digitizer_Analyser/src/buffer.h
@@ -0,0 +1,24 @@
+/*
+ * buffer.h
+ *
+ * Created: 10/02/2021 11:11:09
+ *  Author: decos
+ */ 
+
+
+#ifndef BUFFER_H_
+#define BUFFER_H_
+#include "asf.h"
+
+#include <stdio.h>
+#include <string.h>
+#define buffersize 8334
+
+void printFullBuffer(void);
+void addSample(uint16_t sample);
+volatile void switchBuffer(void);
+void cycleEnded(void);
+uint16_t getbuffersFilled(void);
+void testPrint(void);
+uint16_t* getFilledBuffer(void);
+#endif /* BUFFER_H_ */
