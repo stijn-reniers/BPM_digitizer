94e7004fc9d38e5276d4033b1e83f58f57d4faad
decost67 <decost67@imec.be>
2021-04-05 17:39:09 +0200

    working cpp application

--- BPM_software_application/imgui-master/examples/example_glfw_opengl3/BPM_Reader.cpp
+++ BPM_software_application/imgui-master/examples/example_glfw_opengl3/BPM_Reader.cpp
@@ -9,11 +9,7 @@ unsigned char buf[255];
 bool newTriggerLevel = false, newTriggerDelay = false;
 char tLevel = 255, tDelay = 0;
 
-void resetBuffer(){
-    for (int i = 0;i < 8;i++) {
-        buf[i] = 0;
-    }
-}
+
 //update a certain amount of parameters from a given start index
 void updateParameters(uint8_t amount, uint8_t startingIndex) {
     uint8_t reading = 0;
@@ -49,16 +45,20 @@ int recieveData() {
 
 // send a plot request to the atmel microcontroller and reads the incoming data
 void requestPlot() {
+    RS232_flushRXTX(3);
+    Sleep(100);
+    n=RS232_PollComport(cport_nr, buf, 255);
+    std::cout << n << std::endl;
     RS232_SendByte(cport_nr, 255);
     RS232_SendByte(cport_nr, 3);
     RS232_SendByte(cport_nr, 255);
     //wait for the plot data to become available
     Sleep(2000);
     RS232_PollComport(cport_nr, buf, 3);
-    n = RS232_PollComport(cport_nr, ((unsigned char*)receivedPlot) + 1, 16668);
+    n = RS232_PollComport(cport_nr, ((unsigned char*)receivedPlot), 16668);
     std::cout << n << std::endl;
     newPlotData = true;
-    resetBuffer();
+    
 }
 
 // send a parameter request to the atmel microcontroller and reads the incoming data
@@ -69,7 +69,6 @@ void requestParameters() {
     std::this_thread::sleep_for(std::chrono::microseconds(10));
     RS232_PollComport(cport_nr, buf, 3);
     recieveData();
-    resetBuffer();
 }
 //updates trigger level
 void updateTriggerLevel(int level) {
@@ -81,35 +80,48 @@ void updateTriggerDelay(int delay) {
     newTriggerDelay = true;
     tDelay = (char)delay;
 }
-void changeSettings(char commando, char value, std::string & message) {
+void changeSettings(char commando, char value, std::string * message) {
+    RS232_flushRXTX(3);
+    Sleep(100);
+    n = RS232_PollComport(cport_nr, buf, 255);
+    std::cout << "parameters changed" << std::endl;
+    std::cout << n << std::endl;
     RS232_SendByte(cport_nr, 255);
     RS232_SendByte(cport_nr, commando);
     RS232_SendByte(cport_nr, value);
-    std::this_thread::sleep_for(std::chrono::microseconds(1));
+    std::this_thread::sleep_for(std::chrono::microseconds(10));
     RS232_PollComport(cport_nr, buf, 3);
+    std::cout << "eccho received" << std::endl;
+    for (int i = 0;i < 3;i++) {
+        std::cout << +buf[i] << std::endl;
+    }
     if (buf[0] == 255) {
         if (buf[1] == 0) {
-            message = "eccho message received trigger delay has now been set to = " + std::string(1, buf[2]) + "ms";
+            std::cout << "command recognized" << std::endl;
+            *message = "eccho message received trigger delay has now been set to = ";
+            (*message).append(std::to_string(buf[2]));
+            (*message).append("ms");
         }
-        if (buf[2] == 1) {
-            message = "eccho message received trigger level has now been set to = " + std::string(1, buf[2]);
+        if (buf[1] == 1) {
+            *message = "eccho message received trigger level has now been set to = ";
+           (*message).append(std::to_string(buf[2]));
         }
        
     }   
     else {
-        message = "failed to recognize eccho message";
+        *message = "failed to recognize eccho message";
     }
 }
 // reader thread
-void requestData(std::string & message) {
+void requestData(std::string * message) {
     int i = 0;
     while (running) {
         //request parameter
-        auto start = std::chrono::high_resolution_clock::now();
+        //auto start = std::chrono::high_resolution_clock::now();
         requestParameters();
-        auto stop = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        std::cout << duration.count() << " microseconds" << std::endl;
+        //auto stop = std::chrono::high_resolution_clock::now();
+        //auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+        //std::cout << duration.count() << " microseconds" << std::endl;
 
         //update parameters if necessary
         if (newTriggerDelay) {
@@ -120,18 +132,22 @@ void requestData(std::string & message) {
             changeSettings(1, tLevel, message);
             newTriggerLevel = false;
         }
+       
         //request plot every couple of seconds
         if (i >= 100) {
+            
             std::cout << "plot requested" << std::endl;
-            start = std::chrono::high_resolution_clock::now();
+            //start = std::chrono::high_resolution_clock::now();
             requestPlot();
-            stop = std::chrono::high_resolution_clock::now();
-            duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-            std::cout << duration.count() << " microseconds" << std::endl;
+            //stop = std::chrono::high_resolution_clock::now();
+            //duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+            //std::cout << duration.count() << " microseconds" << std::endl;
 
             i = 0;
         }
+        
         Sleep(50);
+        
         i++;
     }
 }
